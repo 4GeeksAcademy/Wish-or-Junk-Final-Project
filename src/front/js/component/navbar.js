2e907cca62ba05ba0819f3d53802fd81e6aecd9f
Cody Ross <130109458+Rossco1991@users.noreply.github.com>
2023-08-31 00:49:42 +0000

    Built out landing page, able to have navbar and footers incorporated into the home page to match up with wireframe

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,14 +3,34 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-warning bg-gradient">
-			<div className="container">
+		<nav className="navbar bg-transparent">
+			<div className="container-fluid ms-0">
 				<Link to="/">
-					<span className="navbar-brand ms-0">Wish Or Junk</span>
+					<span className="navbar-brand ms-1 btn text-white fw-bold project">Wish Or Junk</span>
 				</Link>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Sign Up</button>
+				<Link to="/home">
+						<button className="text-warning btn ">
+							Home
+						</button>
+					</Link>
+				<Link to="/about-us">
+						<button className="text-warning btn">
+							About Us
+						</button>
+					</Link>
+				<Link to="/posts">
+						<button className="text-warning btn">
+							Posts
+						</button>
+					</Link>
+					<Link to="/shuffle">
+						<button className="text-warning btn"> 
+							Shuffle
+						</button>
+					</Link>
+					<Link to="/sign-up">
+						<button className="btn links">Sign Up</button>
 					</Link>
 				</div>
 			</div>
--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,13 +3,11 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
-import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
+import { Landing } from "./pages/landing";
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
 
 //create your first component
 const Layout = () => {
@@ -23,14 +21,12 @@ const Layout = () => {
         <>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
-                    <Navbar />
                     <Routes>
-                        <Route element={<Home />} path="/" />
+                        <Route element={<Landing />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
-                    <Footer />
                 </ScrollToTop>
             </BrowserRouter>
         </>
