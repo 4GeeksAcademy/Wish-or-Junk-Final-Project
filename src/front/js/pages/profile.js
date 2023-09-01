7d55364ff56d293572ef2d27f18fb6082c3e3581
Robert Bartlett <39223283+Newux@users.noreply.github.com>
2023-09-01 14:24:38 +0000

    Added edit button for profile

--- src/front/js/pages/profile.js
+++ src/front/js/pages/profile.js
@@ -1,35 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Navbar } from "../component/navbar";
 import { Footer } from "../component/footer";
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { UploadImages } from "../component/uploadImages";
+
 
 import "../../styles/profile.css";
 
-export const Profile = () => {
+export const Profile = () => {   
 
     return (
         <div>
             <Navbar />
-            
             <div className="profile-body d-flex justify-content-center">
                 <div className="profile-card">
                     <div className="card-top d-flex">
-                        <p className="profile-heading">Profile</p>
-                        <div className="">
+                        <div className="profile-edit-group">
+                            <p className="profile-heading">Profile</p>
+                            <a type="button" className="profile-edit">Edit<i className="fa-regular fa-pen-to-square ms-2"></i></a>
+                        </div>
+                        <div className="profile-pic-group">
                             <div className="profile-pic-orbit">
                             <div className="profile-pic">
-                                <i className="add-pic-icon fa-solid fa-circle-plus fa-2xl" style={{color: "blue",}}></i>
+                                <i type="button" className="add-pic-icon fa-solid fa-circle-plus fa-2xl" style={{color: "blue",}}></i>
                             </div>
                             </div>
                         </div>
                     </div>
                     <div className="card-mid d-flex">
                         <div className="settings">
-                            <p>Name</p>
-                            <p>email</p>
-                            <p>Username</p>
-                            <p>Number of posts</p>
+                            <p className="settings-title text-wrap">Name</p>
+                            <p className="settings-title text-wrap">Email</p>
+                            <p className="settings-title text-wrap">Username</p>
+                            <p className="settings-title text-wrap">Number of posts</p>
                         </div>
                         <div className="mission-statement text-wrap px-3 pt-3">
                             <h5>Mission Statement</h5>
@@ -41,10 +43,12 @@ export const Profile = () => {
                         </div>
                     </div>
                     <div className="social-media d-flex justify-content-end">
-                        <a type="button" className="btn"><i className="fa-brands fa-instagram fa-xl"/></a>   
-                        <a type="button" className="btn"><i className="fa-brands fa-twitter fa-xl"/></a>
-                        <a type="button" className="btn"><i className="fa-brands fa-facebook-f fa-xl px-3"/></a>
-                        <a type="button" className="btn"><i className="lin fa-brands fa-linkedin-in fa-xl"/></a>
+                        <a type="button" href="https://www.instagram.com/"className="btn"><i className="fa-brands fa-instagram fa-xl"/></a>   
+                        <a type="button" href="https://twitter.com/?lang=en" className="btn"><i className="fa-brands fa-twitter fa-xl"/></a>
+                        <a type="button" href="https://www.facebook.com/" className="btn"><i className="fa-brands fa-facebook-f fa-xl px-3"/></a>
+                        <a type="button" href="https://www.linkedin.com/?trk=guest_homepage-basic_nav-header-logo" 
+                        className="btn"><i className="lin fa-brands fa-linkedin-in fa-xl"/>
+                        </a>
                     </div>    
                 </div>
             </div>
