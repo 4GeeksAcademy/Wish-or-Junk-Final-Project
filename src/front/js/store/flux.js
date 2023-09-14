d424d5579d2e5ff5a18692efa1fab7ee0fe8135a
magic891103 <133174388+magic891103@users.noreply.github.com>
2023-09-14 23:32:01 +0000

    second try.

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,86 +1,109 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+    return {
+        store: {
+            message: null,
+            demo: [
+                {
+                    title: "FIRST",
+                    background: "white",
+                    initial: "white"
+                },
+                {
+                    title: "SECOND",
+                    background: "white",
+                    initial: "white"
+                }
+            ],
+            user: null,
+            token: null
+        },
+        actions: {
+            exampleFunction: () => {
+                getActions().changeColor(0, "green");
+            },
 
-			getMessage: async () => {
-				try {
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				} catch (error) {
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+            getMessage: async () => {
+                try {
+                    const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
+                    const data = await resp.json();
+                    setStore({ message: data.message });
+                    return data;
+                } catch (error) {
+                    console.log("Error loading message from backend", error);
+                }
+            },
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+            changeColor: (index, color) => {
+                const store = getStore();
+                const demo = store.demo.map((elm, i) => {
+                    if (i === index) elm.background = color;
+                    return elm;
+                });
+                setStore({ demo: demo });
+            },
 
-				//reset the global store
-				setStore({ demo: demo });
-			},
-			login: async (email, password) => {
-				try {
-					const resp = await fetch(process.env.BACKEND_URL + "/api/login", {
-						method: "POST",
-						headers: {
-							"Content-Type": "application/json",
-						},
-						body: JSON.stringify({
-							email: email,
-							password: password
-						}),
-					});
+            login: async (email, password) => {
+                try {
+                    const resp = await fetch(process.env.BACKEND_URL + "/api/login", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            email: email,
+                            password: password
+                        }),
+                    });
 
-					if (!resp.ok) {
-						throw new Error("Failed to login");
-					}
+                    if (!resp.ok) {
+                        throw new Error("Failed to login");
+                    }
 
-					const data = await resp.json();
-					setStore({
-						user: data.user,
-						token: data.token
-					});
-				} catch (error) {
-					console.log("Error during login:", error);
-				}
-			},
-			logout: () => {
-				setStore({
-					user: null,
-					token: null
-				});
-			}
-		}
-	};
+                    const data = await resp.json();
+                    setStore({
+                        user: data.user,
+                        token: data.token
+                    });
+                } catch (error) {
+                    console.log("Error during login:", error);
+                }
+            },
+
+            logout: () => {
+                setStore({
+                    user: null,
+                    token: null
+                });
+            },
+
+            signup: async (email, password) => {
+                try {
+                    const resp = await fetch(process.env.BACKEND_URL + "/api/signup", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            email: email,
+                            password: password
+                        }),
+                    });
+
+                    if (!resp.ok) {
+                        throw new Error("Failed to sign up");
+                    }
+
+                    const data = await resp.json();
+                    setStore({
+                        user: data.user,
+                        token: data.token
+                    });
+                } catch (error) {
+                    console.log("Error during signup:", error);
+                }
+            }
+        }
+    };
 };
 
 export default getState;
