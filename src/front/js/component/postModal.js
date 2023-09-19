7fe120c844c273dc2f79abb4e9b7ee4df72617d5
eXcapert <130175463+eXcapert@users.noreply.github.com>
2023-09-19 00:49:09 +0000

    Fixed bug in Post Modal not allowing use of any button or text field. Updated to not dismiss when clicking background of window, must click close button on the modal.

--- src/front/js/component/postModal.js
+++ src/front/js/component/postModal.js
@@ -7,15 +7,17 @@ export const PostModal = () => {
 
     const picURL = "https://star-name-registry.com/blog/images/d/0/1/f/a/d01faec7ef04415eec34c1bfe61913e167fb26c7-snr-blog-37-resized.jpg";
     return (
-        <div className="container d-flex justify-content-center col-6">
-            <button type="button" className="btn btn-warning d-grid col-6"
-                data-bs-toggle="modal" data-bs-target="#exampleModal"
-                data-bs-whatever="@getbootstrap">CREATE POST
-            </button>
+        <div>
+            <div className="container d-flex justify-content-center col-6">
+                <button type="button" className="btn btn-warning d-grid col-6"
+                    data-bs-toggle="modal" data-bs-target="#exampleModal"
+                    data-bs-whatever="@getbootstrap">CREATE POST
+                </button>
+            </div>
             <div className="modal fade" id="exampleModal" tabindex="-1"
-                aria-labelledby="exampleModalLabel" aria-hidden="true">
+                aria-labelledby="exampleModalLabel" aria-hidden="true" data-bs-backdrop="static">
                 <div className="modal-dialog">
-                    <div className="post-modal-content">
+                    <div className="post-modal-content modal-content">
                         <div className="modal-header">
                             <h1 className="modal-title fs-5" id="exampleModalLabel">New Post</h1>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
